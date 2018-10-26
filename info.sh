b829eddc8461120ba595f02a6524d9c2f78d36d0
Slava <kuldur196932@gmail.com>
2018-10-26 22:51:14 +0300

    added cpu and memory info

---
+++ info.sh
@@ -0,0 +1,17 @@
+#bin/bash
+echo "====================Start script==================== $0"
+os=`uname -a`
+mem=`cat /proc/meminfo`
+cpu=`cat /proc/cpuinfo`
+echo "operating system is==== $os======"
+echo "memory is======== $mem========"
+
+
+
+
+
+
+
+
+
+echo "====================End script===================  $0 "
