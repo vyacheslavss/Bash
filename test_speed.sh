a3d6619f71d93ff5776d54e23308d20db100a03a
vyacheslavss <32959613+vyacheslavss@users.noreply.github.com>
2023-02-04 14:49:53 +0200

    Fixed grammar

--- test_speed.sh
+++ test_speed.sh
@@ -2,6 +2,6 @@
 echo "=============Start testing internet speed=====================$0"
 date=`date`
 speed=`speedtest-cli`
-echo "The speed test wos at===$date"
+echo "The speed test was at===$date"
 echo "speed is $speed"  
 echo "=============End testing internet speed========================="
